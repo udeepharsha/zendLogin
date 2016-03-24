3c4812ab6ca8c91d8131326db2f2472a6a6713a5
udeepharsha <udeepharsha@gmail.com>
2016-03-24 13:37:20 -0700

    first commit

---
+++ config/autoload/global.php
@@ -0,0 +1,18 @@
+<?php
+return array(
+    'db' => array(
+        'driver'         => 'Pdo',
+        'dsn'            => 'mysql:dbname=zf2login;host=localhost',
+        'driver_options' => array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+        ),
+        'username' => 'root',
+        'password' => '',
+    ),
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter'
+                    => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ),
+    ),
+);
